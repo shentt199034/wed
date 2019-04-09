4a835d09c51d920a45bbbce38557f6e404ac3320
Administrator <Administrator@ZINBE9J8ZJN5WPT>
2019-04-09 16:01:18 +0800

    service已经修改完成！

--- src/cn/hw/oa/service/Productservice.java
+++ src/cn/hw/oa/service/Productservice.java
@@ -1,15 +1,19 @@
 package cn.hw.oa.service;
 //业务逻辑层（JAVA类），以后添加购物车的业务逻辑层
-
-import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import cn.hw.oa.dao.Productdao;
 import model.Product;
 
 public class Productservice {
 //执行完业务逻辑才调用数据访问层入库
-	private Productdao productdao=new Productdao();
+	private Productdao productdao=null;
+	public void setProductdao(Productdao productdao) {
+		this.productdao = productdao;
+	}
 	public void save (Product product) {
 		productdao.save(product);
 	}
@@ -25,5 +29,21 @@ public class Productservice {
 	public List<Product> queryByName(String name) {
 		return productdao.queryByName(name);
 	}
+	public static void main(String[] args) {
+		// new的对象并不是通过spring框架创建,因此没有依赖注入的功能, 因此只能通过Spring创建
+		// ProductDao productDao = new ProductDao();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-bean.xml");
+		// 通过id获取相应的类型，并且创建对象(通过spring配置的bean才拥有依赖注入的功能)
+		Productservice productservice = context.getBean("ps", Productservice.class);
+		// 项目中所有的model不需要交给spring管理,因为这些值是从前端传入,而且是多例模式
+		Product product = new Product();
+		product.setName("xyz");
+		productservice.save(product);
+		List<Product> prolist=productservice.queryByName("");
+		for(Product temp : prolist) {
+			System.out.println(temp);
+		}
+		
+		}
  
 	}
